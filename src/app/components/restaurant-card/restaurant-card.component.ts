7ba3bea5c817003da533b8bb160bd19cf376ecb9
Risshab Singla <risshab2singla@gmail.com>
2024-11-05 19:33:26 +0530

    Restaurant List Homepage

--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-restaurant-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './restaurant-card.component.css'
 })
 export class RestaurantCardComponent {
-
+  @Input({required:true}) name!: string;
+  @Input({required:true}) imageSrc!: string;
+  @Input({required:true}) location!: string;
+  @Input({required:true}) cuisines!: string[];
+  @Input({required:true}) rating!: number;
+  @Input({required:true}) price!: string;
 }
