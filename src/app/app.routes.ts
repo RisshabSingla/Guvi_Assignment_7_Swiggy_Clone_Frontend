0f67de4389a5330cf1e90b2b6d3737b2f4912909
Risshab Singla <risshab2singla@gmail.com>
2024-11-05 12:43:48 +0530

    Component Setup and Routing Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RestaurantDetailsComponent } from './pages/restaurant-details/restaurant-details.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'restaurant/:id', component: RestaurantDetailsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/home' }, 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
