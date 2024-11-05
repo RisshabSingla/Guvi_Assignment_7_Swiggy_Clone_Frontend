13f2decec38d7d1613020638e34b037cca3aa9bd
Risshab Singla <risshab2singla@gmail.com>
2024-11-05 23:22:24 +0530

    Changed ID's for better Cart Management

--- src/app/dummyItemsList.ts
+++ src/app/dummyItemsList.ts
@@ -1,97 +1,102 @@
-export const dummyItems  = [
+export const dummyItems = [
     {
-        id: 1,
+        id: "f1g2h3j", // Random ID
         name: "Item 1",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/0a688af1-1bb4-4a55-8128-31fc79cc9ad0_6d0abb9a-daff-4fbe-a1c9-2dddb6ae6717"
     },
     {
-        id: 2,
+        id: "a2b3c4d", // Random ID
         name: "Item 2",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/85df9d8f-175f-4e3a-8945-468bf6317eee_eb9bf247-f2d1-413d-9cf5-48bc870b222f"
     },
     {
-        id: 3,
+        id: "e5f6g7h", // Random ID
         name: "Item 3",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/afd63459-b4f5-40ef-bb77-a0b8c09b81b9_c242fdcb-6dc2-41d1-9575-f89ca6cfdb99"
     },
     {
-        id: 4,
+        id: "i8j9k0l", // Random ID
         name: "Item 4",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/90d94613-6a3f-4f86-b0ce-bff84443f18e_1934c92e-b882-463d-95f8-8a6b9a2aeb78"
     },
     {
-        id: 5,
+        id: "m1n2o3p", // Random ID
         name: "Item 5",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/f8ad7bfd-130e-4809-b5aa-3c9c16f931ec_27153c0e-ca9c-4df1-baa7-96dc561a718f"
     },
     {
-        id: 6,
+        id: "q4r5s6t", // Random ID
         name: "Item 6",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/b1b541ac-2034-4687-aa02-3d58b42c40d7_2c7514ec-95b8-41f5-97a1-4a11670b2120"
     },
     {
-        id: 7,
+        id: "u7v8w9x", // Random ID
         name: "Item 7",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/2ead57e4-00b3-4525-91b2-9eb37a4be375_eee95c8a-e736-42ed-ab86-46add6ffe374"
     },
     {
-        id: 8,
+        id: "y0z1a2b", // Random ID
         name: "Item 8",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/9314636d-dcb2-468b-bdad-9170f4133e14_00e24371-61a6-4ab5-98e9-552bb94d2557"
     },
     {
-        id: 9,
+        id: "c3d4e5f", // Random ID
         name: "Item 9",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/7/18/84e4f7d5-03ab-42e2-8765-e05ecfd9d925_212a82ac-67ed-4ff1-b471-ee622b71d404"
     },
     {
-        id: 10,
+        id: "g6h7i8j", // Random ID
         name: "Item 10",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/c1d3779d-9b01-4853-8cad-99185e39c933_a0e15a76-ea59-47e6-a98e-0bb4dfc40600"
     },
     {
-        id: 11,
+        id: "k9l0m1n", // Random ID
         name: "Item 11",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/67be61b3-99b4-4037-823c-a2cf0155b0af_00bd4c81-9099-4868-be16-0fdeb382a8dc"
     },
     {
-        id: 12,
+        id: "o2p3q4r", // Random ID
         name: "Item 12",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/9/4e5eec2b-4797-4a93-989a-71b0f5958a3f_cae7e951-fc56-4886-86dd-f583e2373c52"
     },
     {
-        id: 13,
+        id: "s5t6u7v", // Random ID
         name: "Item 13",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/e33d8321-cb8c-4732-9149-beb834eb24e4_8cee91b0-a380-43bb-90cf-9346a322ebeb"
     },
     {
-        id: 14,
+        id: "w8x9y0z", // Random ID
         name: "Item 14",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/7c574228-ae92-4199-a45b-dfc3229c09e7_69a4ac73-d9f5-498b-a5cc-40afd90ee4b0"
     },
     {
-        id: 15,
+        id: "a1b2c3d", // Random ID
         name: "Item 15",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/e50a580b-4bd7-449a-af9e-fbf01ea18843_e5e21b56-781f-4ccd-b431-a343a23fecb3"
     },
     {
-        id: 16,
+        id: "e4f5g6h", // Random ID
         name: "Item 16",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/ed1b860f-4fc6-421e-ace8-3c2bde3fc676_3404cf52-d2ad-43db-a349-0846871ddcfa"
     },
     {
-        id: 17,
+        id: "i7j8k9l", // Random ID
         name: "Item 17",
         imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/977de398-06fe-4d77-bc77-55158c438dad_9aa7f78a-9590-4625-88e7-520cc29ae3f7"
     },
     {
-        id: 18,
+        id: "m0n1o2p", // Random ID
         name: "Item 18",
-        imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/b3d0d949-d42c-46e0-b0e7-b1176904cbac_a1787414-2095-4430-b8c0-50783953caf8"
+        imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/06e92959-1fb5-4642-8438-8c113ab988c0_e0a29948-b111-4936-9b4b-05e6865b40c7"
     },
     {
-        id: 19,
+        id: "q3r4s5t", // Random ID
         name: "Item 19",
-        imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/5/9/61933ca1-f2fa-443a-910d-feb2ab6ecf79_c849fce4-cdab-4317-8fb5-b6dff377dba9"
+        imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/2/8aa60592-0e82-4741-a194-c2a514cfa5aa_b2b8820e-64af-49a1-9fd8-1f68f21d3f24"
     },
+    {
+        id: "u6v7w8x", // Random ID
+        name: "Item 20",
+        imageSrc: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/NI_CATALOG/IMAGES/CIW/2024/4/22/0e37be69-d7f6-43c8-8f57-c894eb3948be_6fa84ae2-fd3c-4661-8013-06407e1f7154"
+    }
 ];
