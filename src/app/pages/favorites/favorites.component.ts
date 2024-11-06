c2ab687cbbb85f3a5d3e2f435bc4210229408afd
Risshab Singla <risshab2singla@gmail.com>
2024-11-06 14:31:46 +0530

    Adding from the favourites to the cart

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FavoritesService, FavoriteItem } from '../../services/favorites.service'
+import { CartService, CartItem } from '../../services/cart.service'; 
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
 export class FavoritesComponent implements OnInit {
   favoriteItems: FavoriteItem[] = [];
 
-  constructor(private favoritesService: FavoritesService) {}
+  constructor(
+    private favoritesService: FavoritesService,
+    private cartService: CartService  
+  ) {}
 
   ngOnInit() {
     this.favoritesService.favoriteItems$.subscribe(items => {
@@ -20,10 +26,22 @@ export class FavoritesComponent implements OnInit {
   }
 
   removeFromFavorites(id: string) {
-    // Find the item to remove based on id and remove it
     const itemToRemove = this.favoriteItems.find(item => item.id === id);
     if (itemToRemove) {
       this.favoritesService.toggleFavorite(itemToRemove);
     }
+  }
+
+  addToCart(item: FavoriteItem) {
+    const cartItem: CartItem = {
+      id: item.id,
+      foodItemName: item.foodItemName,
+      imageSource: item.imageSource,
+      quantity: 1,
+      price: item.price
+    };
+
+    this.cartService.addToCart(cartItem); 
+    console.log(`Added ${item.foodItemName} to cart.`);
   }
 }
